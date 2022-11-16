51006627f106fca6245ff4e6a293f5597efc387f
mirza shamsudin <mirza_shamsudin@hotmail.com>
2022-11-16 13:36:24 +0800

    added_content_column

--- db/migrate/20221116053346_add_content_column.rb
+++ db/migrate/20221116053346_add_content_column.rb
@@ -1,4 +1,4 @@
-class AddContentToProducts < ActiveRecord::Migration[7.0]
+class AddContentColumn < ActiveRecord::Migration[7.0]
   def change
     add_column :products, :content, :string, null: false
   end
