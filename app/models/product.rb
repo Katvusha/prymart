312c286b1e3331e4c46e90ecbd16e31e32c90b20
mirza shamsudin <mirza_shamsudin@hotmail.com>
2022-11-16 14:25:20 +0800

    Added validations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :content, presence: true, uniqueness: true
 end
