56ea3b947a05feb7786a6e2099bdc491a2063ce8
mirza shamsudin <mirza_shamsudin@hotmail.com>
2022-11-16 15:28:00 +0800

    adding-gem-dotenv-rails-and-cloudinary-setup

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_one_attached :photo
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :content, presence: true, uniqueness: true
 end
