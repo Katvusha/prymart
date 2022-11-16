b765838e117a798683201908dee7050221b9a821
Jun Jie <112390808+junjietdl@users.noreply.github.com>
2022-11-16 10:15:26 +0800

    Deleted application_controller.rb

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  def index
-    @offers = []
-
-    current_user.products.each do |product|
-      @offers << product.offers
-    end
-  end
-end
