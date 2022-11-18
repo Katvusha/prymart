7457e6d657dd7384b81cf3425ed0e2d4ddc0daf1
Jun Jie Teo <junjietdl@gmail.com>
2022-11-18 13:23:46 +0800

    added user product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_18_051249) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_051453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_051249) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_products", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_user_products_on_product_id"
+    t.index ["user_id"], name: "index_user_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -93,4 +102,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_051249) do
   add_foreign_key "offers", "products"
   add_foreign_key "offers", "users"
   add_foreign_key "products", "users"
+  add_foreign_key "user_products", "products"
+  add_foreign_key "user_products", "users"
 end
